5514169567654e96821968408459fb5f74b4a175
Karinis24 <evelynkarinah09@gmail.com>
2025-08-26 17:45:12 -0500

    esto es otro grito de ayuda

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //Alejandro gooooood Andres goooood William zzzzz
+
+//karinis24 se me metió al rancho ayuda
